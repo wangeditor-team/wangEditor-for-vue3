e4642d7e087b7c3f3c1f168bd4590503109d844f
Liu <528851597@qq.com>
2021-09-11 17:35:56 +0800

    fix: 修复script module模式下toolbar无法初始化的问题

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,10 +1,21 @@
-
-<script lang='ts'>
-import { h, onMounted, defineComponent, ref, onUnmounted, PropType } from 'vue'
-import { createToolbar, IToolbarConfig, Toolbar } from '@wangeditor/editor-cattle'
-import emitter from '../utils/emitter'
-import { divide } from 'lodash'
-import { getEditor, removeEditor } from '../utils/editor-map'
+<template>
+  <div ref="selector" classname="w-e-toolbar-init"></div>
+</template>
+<script lang="ts">
+import {
+  defineComponent,
+  ref,
+  onUnmounted,
+  PropType,
+  onBeforeMount,
+} from "vue";
+import {
+  createToolbar,
+  IDomEditor,
+  IToolbarConfig,
+} from "@wangeditor/editor-cattle";
+import emitter from "../utils/emitter";
+import { getEditor } from "../utils/editor-map";
 
 export default defineComponent({
   props: {
@@ -16,7 +27,7 @@ export default defineComponent({
     /** 编辑器模式 */
     mode: {
       type: String,
-      default: 'default',
+      default: "default",
     },
     /** 编辑器默认配置 */
     defaultConfig: {
@@ -26,44 +37,39 @@ export default defineComponent({
   },
   setup(props) {
     // 编辑器容器
-    const selector = ref(null)
+    const selector = ref(null);
     /**
      * 初始化编辑器
      */
-    const initToolbar = () => {
-      if (!selector.value) return
+    const initToolbar = (editor: IDomEditor) => {
+      if (!selector.value) return;
+      editor = editor || getEditor(props.editorId);
+      if (editor == null) {
+        throw new Error(
+          "Not found instance of Editor when create <Toolbar/> component"
+        );
+      }
+      createToolbar({
+        editor,
+        selector: (selector.value! as Element) || "<div></div>",
+        mode: props.mode,
+        config: props.defaultConfig,
+      });
+    };
 
-      // 监听编辑器创建成功的事件，创建成功后创建对应的toolbar
-      emitter.on(`w-e-created-${props.editorId}`, editor => {
-        // 如果编辑器实例未初始化，则抛出错误
-        if (editor == null) {
-          throw new Error('Not found instance of Editor when create <Toolbar/> component')
-        }
-
-        createToolbar({
-          editor,
-          selector: selector.value! as Element || '<div></div>',
-          mode: props.mode,
-          config: props.defaultConfig,
-        })
-      })
-    }
-
-    onMounted(() => {
-      initToolbar()
-    })
+    onBeforeMount(() => {
+      emitter.on(`w-e-created-${props.editorId}`, (editor) => {
+        initToolbar(editor);
+      });
+    });
 
     onUnmounted(() => {
-      emitter.off(`w-e-created-${props.editorId}`, initToolbar)
-    })
+      emitter.off(`w-e-created-${props.editorId}`, initToolbar);
+    });
 
-    return () =>
-      h('div', {
-        ref: selector
-      })
+    return {
+      selector,
+    };
   },
-})
-
-
+});
 </script>
-
