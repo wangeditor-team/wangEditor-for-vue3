5d03b3c68b095aadcfb68a2eb8f78a134d45bb43
liuqh <liuqinghua.lqh@alibaba-inc.com>
2021-09-18 17:28:22 +0800

    fix: 修改打包配置,排除核心包

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,34 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts'
-import path from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
+import path from 'path';
 
-const name = 'WangEditorForVue'
-const entry = path.resolve(__dirname, './src', 'index.ts')
+const name = 'WangEditorForVue';
+const entry = path.resolve(__dirname, './src', 'index.ts');
 
 export default defineConfig({
-
   build: {
     sourcemap: true,
     lib: {
       entry,
       name: name,
-      fileName: (format) => `index.${format}.js`
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue', '@wangeditor/editor-cattle'],
+      external: ['vue', '@wangeditor/editor'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
-          vue: 'Vue'
-        }
-      }
+          vue: 'Vue',
+        },
+      },
     },
   },
-  plugins: [vue(), dts({
-    exclude: ['./src/App.vue', './src/main.ts']
-  })],
-})
+  plugins: [
+    vue(),
+    dts({
+      exclude: ['./src/App.vue', './src/main.ts'],
+    }),
+  ],
+});
