d1d9ac3b80430b01cb6b557fb8c87c0ea5f6bd67
clinfc <1962109568@qq.com>
2022-03-07 22:10:05 +0800

    feat: 内置 deep clone

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -3,149 +3,130 @@
 </template>
 
 <script lang="ts">
-import { onMounted, defineComponent, ref, PropType, onUnmounted } from "vue";
-import { createEditor, IEditorConfig } from "@wangeditor/editor";
-import { Descendant } from "slate";
-import { getEditor, recordEditor, removeEditor } from "../utils/editor-map";
-import { genErrorInfo } from "../utils/create-info";
-import emitter from "../utils/emitter";
+  import { onMounted, defineComponent, ref, PropType, onUnmounted, toRaw } from 'vue'
+  import { createEditor, IEditorConfig } from '@wangeditor/editor'
+  import { Descendant } from 'slate'
+  import { getEditor, recordEditor, removeEditor } from '../utils/editor-map'
+  import { genErrorInfo } from '../utils/create-info'
+  import emitter from '../utils/emitter'
 
-export default defineComponent({
-  props: {
-    /** 编辑器默认ID */
-    editorId: {
-      type: String,
-      required: true,
+  export default defineComponent({
+    props: {
+      /** 编辑器默认ID */
+      editorId: {
+        type: String,
+        required: true,
+      },
+      /** 编辑器模式 */
+      mode: {
+        type: String,
+        default: 'default',
+      },
+      /** 编辑器默认内容 */
+      defaultContent: {
+        type: Array as PropType<Descendant[]>,
+        default: () => [],
+      },
+      defaultHtml: {
+        type: String,
+        default: '',
+      },
+      /** 编辑器默认配置 */
+      defaultConfig: {
+        type: Object as PropType<Partial<IEditorConfig>>,
+        default: () => ({}),
+      },
     },
-    /** 编辑器模式 */
-    mode: {
-      type: String,
-      default: "default",
-    },
-    /** 编辑器默认内容 */
-    defaultContent: {
-      type: Array as PropType<Descendant[]>,
-      default: [],
-    },
-    defaultHtml: {
-      type: String,
-      default: '',
-    },
-    /** 编辑器默认配置 */
-    defaultConfig: {
-      type: Object as PropType<Partial<IEditorConfig>>,
-      default: {},
-    },
-  },
-  created() {
-    // 检查用户是否传入了editorId
-    if (this.editorId == null) {
-      throw new Error("Need `editorId` props when create <Editor/> component");
-    }
-  },
-  setup(props, context) {
-    // 编辑器容器
-    const box = ref(null);
-    /**
-     * 初始化编辑器
-     */
-    const initEditor = () => {
-      if (!box.value) return;
+    setup(props, { emit }) {
+      // 检查用户是否传入了editorId
+      if (!props.editorId) {
+        throw new Error('Need `editorId` props when create <Editor/> component')
+      }
 
-      createEditor({
-        selector: box.value! as Element,
-        mode: props.mode,
-        content: props.defaultContent || [],
-        html: props.defaultHtml || '',
-        config: {
-          ...props.defaultConfig,
-          onCreated(editor) {
-            // 记录 editor
-            recordEditor(props.editorId, editor);
-            // 触发自定义事件（如创建 toolbar）
-            emitter.emit(`w-e-created-${props.editorId}`, editor);
-            context.emit("onCreated", editor);
+      // 编辑器容器
+      const box = ref(null)
 
-            if (props.defaultConfig.onCreated) {
-              const info = genErrorInfo("onCreated");
-              throw new Error(info);
-            }
-          },
-          onChange(editor) {
-            context.emit("onChange", editor);
-            if (props.defaultConfig.onChange) {
-              const info = genErrorInfo("onChange");
-              throw new Error(info);
-            }
-          },
-          onDestroyed(editor) {
-            context.emit("onDestroyed", editor);
-            if (props.defaultConfig.onDestroyed) {
-              const info = genErrorInfo("onDestroyed");
-              throw new Error(info);
-            }
-          },
-          onMaxLength(editor) {
-            context.emit("onMaxLength", editor);
-            if (props.defaultConfig.onMaxLength) {
-              const info = genErrorInfo("onMaxLength");
-              throw new Error(info);
-            }
-          },
-          onFocus(editor) {
-            context.emit("onFocus", editor);
-            if (props.defaultConfig.onFocus) {
-              const info = genErrorInfo("onFocus");
-              throw new Error(info);
-            }
-          },
-          onBlur(editor) {
-            context.emit("onBlur", editor);
-            if (props.defaultConfig.onBlur) {
-              const info = genErrorInfo("onBlur");
-              throw new Error(info);
-            }
-          },
-          customAlert(info, type) {
-            context.emit("customAlert", info, type);
-            // @ts-ignore
-            if (props.defaultConfig.customAlert) {
-              const info = genErrorInfo("customAlert");
-              throw new Error(info);
-            }
-          },
-          customPaste: (editor, event): any => {
-            if (props.defaultConfig.customPaste) {
-              const info = genErrorInfo("customPaste");
-              throw new Error(info);
-            }
-            let res;
-            context.emit("customPaste", editor, event, (val: boolean) => {
-              res = val;
-            });
-            return res;
-          },
-        },
-      });
-    };
-    /**
-     * 元素挂在后初始化编辑器
-     */
-    onMounted(() => {
-      initEditor();
-    });
+      function validate(key: keyof IEditorConfig) {
+        if (typeof props.defaultConfig[key] === 'function') {
+          throw new Error(genErrorInfo(key))
+        }
+      }
 
-    onUnmounted(() => {
-      const editor = getEditor(props.editorId);
-      if (editor == null) return;
-      // 销毁，并移除 editor
-      editor.destroy();
-      removeEditor(props.editorId);
-    });
+      /**
+       * 初始化编辑器
+       */
+      const initEditor = () => {
+        if (!box.value) return
 
-    return {
-      box,
-    };
-  },
-});
+        // 获取原始数据，解除响应式特性
+        const { editorId, mode, defaultConfig, defaultContent, defaultHtml } = toRaw(props)
+
+        createEditor({
+          selector: box.value,
+          mode: mode,
+          content: defaultContent,
+          html: defaultHtml,
+          config: {
+            ...defaultConfig,
+            onCreated(editor) {
+              // 记录 editor
+              recordEditor(editorId, editor)
+              // 触发自定义事件（如创建 toolbar）
+              emitter.emit(`w-e-created-${editorId}`, editor)
+              emit('onCreated', editor)
+              validate('onCreated')
+            },
+            onChange(editor) {
+              emit('onChange', editor)
+              validate('onChange')
+            },
+            onDestroyed(editor) {
+              emit('onDestroyed', editor)
+              validate('onDestroyed')
+            },
+            onMaxLength(editor) {
+              emit('onMaxLength', editor)
+              validate('onMaxLength')
+            },
+            onFocus(editor) {
+              emit('onFocus', editor)
+              validate('onFocus')
+            },
+            onBlur(editor) {
+              emit('onBlur', editor)
+              validate('onBlur')
+            },
+            customAlert(info, type) {
+              emit('customAlert', info, type)
+              validate('customAlert')
+            },
+            customPaste(editor, event): any {
+              validate('customPaste')
+              let res
+              emit('customPaste', editor, event, (val: boolean) => {
+                res = val
+              })
+              return res
+            },
+          },
+        })
+      }
+      /**
+       * 元素挂在后初始化编辑器
+       */
+      onMounted(initEditor)
+
+      onUnmounted(() => {
+        const editor = getEditor(props.editorId)
+        if (editor == null) return
+        // 销毁，并移除 editor
+        editor.destroy()
+        removeEditor(props.editorId)
+      })
+
+      return {
+        box,
+      }
+    },
+  })
 </script>
--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -2,70 +2,59 @@
   <div ref="selector" classname="w-e-toolbar-init"></div>
 </template>
 <script lang="ts">
-import {
-  defineComponent,
-  ref,
-  onUnmounted,
-  PropType,
-  onBeforeMount,
-} from "vue";
-import { createToolbar, IDomEditor, IToolbarConfig } from "@wangeditor/editor";
-import emitter from "../utils/emitter";
-import { getEditor } from "../utils/editor-map";
+  import { defineComponent, ref, onUnmounted, PropType, onBeforeMount } from 'vue'
+  import { createToolbar, IDomEditor, IToolbarConfig } from '@wangeditor/editor'
+  import emitter from '../utils/emitter'
+  import { getEditor } from '../utils/editor-map'
 
-export default defineComponent({
-  props: {
-    /** 编辑器默认ID */
-    editorId: {
-      type: String,
-      required: true,
+  export default defineComponent({
+    props: {
+      /** 编辑器默认ID */
+      editorId: {
+        type: String,
+        required: true,
+      },
+      /** 编辑器模式 */
+      mode: {
+        type: String,
+        default: 'default',
+      },
+      /** 编辑器默认配置 */
+      defaultConfig: {
+        type: Object as PropType<Partial<IToolbarConfig>>,
+        default: () => ({}),
+      },
     },
-    /** 编辑器模式 */
-    mode: {
-      type: String,
-      default: "default",
-    },
-    /** 编辑器默认配置 */
-    defaultConfig: {
-      type: Object as PropType<Partial<IToolbarConfig>>,
-      default: {},
-    },
-  },
-  setup(props) {
-    // 编辑器容器
-    const selector = ref(null);
-    /**
-     * 初始化编辑器
-     */
-    const initToolbar = (editor: IDomEditor) => {
-      if (!selector.value) return;
-      editor = editor || getEditor(props.editorId);
-      if (editor == null) {
-        throw new Error(
-          "Not found instance of Editor when create <Toolbar/> component"
-        );
+    setup(props) {
+      // 编辑器容器
+      const selector = ref(null)
+
+      /**
+       * 初始化编辑器
+       */
+      const initToolbar = (editor: IDomEditor) => {
+        if (!selector.value) return
+        editor = editor || getEditor(props.editorId)
+        if (editor == null) {
+          throw new Error('Not found instance of Editor when create <Toolbar/> component')
+        }
+        createToolbar({
+          editor,
+          selector: selector.value || '<div></div>',
+          mode: props.mode,
+          config: props.defaultConfig,
+        })
       }
-      createToolbar({
-        editor,
-        selector: (selector.value! as Element) || "<div></div>",
-        mode: props.mode,
-        config: props.defaultConfig,
-      });
-    };
 
-    onBeforeMount(() => {
-      emitter.on(`w-e-created-${props.editorId}`, (editor) => {
-        initToolbar(editor);
-      });
-    });
+      emitter.on(`w-e-created-${props.editorId}`, initToolbar)
 
-    onUnmounted(() => {
-      emitter.off(`w-e-created-${props.editorId}`, initToolbar);
-    });
+      onUnmounted(() => {
+        emitter.off(`w-e-created-${props.editorId}`, initToolbar)
+      })
 
-    return {
-      selector,
-    };
-  },
-});
+      return {
+        selector,
+      }
+    },
+  })
 </script>
